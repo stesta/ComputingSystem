8aa95bb31275a6b556baafd530da33b056cd8576
Steven Testa <steve@testasoftware.com>
2017-01-04 21:58:14 -0500

    began a working test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+import ElementaryGates
+import ALU
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "alu" $ do
+    it "zx:0, nx:0, zy:1, ny:1, f:0, no:0 (x)" $ do
+      (alu (0,0,1,1,0,0) [0,0,1,1] [0,1,0,1]) `shouldBe` [0,0,1,1]
