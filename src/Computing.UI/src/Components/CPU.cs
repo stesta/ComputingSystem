ab51f4ab6f94759f39e1d1e70064f2afd3efd98b
Steve Testa <steve@testasoftware.com>
2025-09-13 14:33:10 -0400

    added the CPU, load a basic addition program in ROM, cleanup

--- src/Computing.UI/src/Components/CPU.cs
+++ src/Computing.UI/src/Components/CPU.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ public class ProgramCounter
 {
     private int _value = 0;
 
-    public int Tick(int input, int load, int inc, int reset)
+    public int Compute(int input, int load, int inc, int reset)
     {
         if (reset != 0)
         {
@@ -35,18 +36,80 @@ public class CPU
     public int D = new();
     public int PC = new();
 
+    private ProgramCounter _pc = new();
+
     private bool toggle = false;
 
-    // TODO: need to actually implement the CPU
-    public void Execute(int instruction, RAM ram)
+    public void Execute(int instruction, RAM M)
     {
-        int value = toggle ? 0xFFFF : 0x0000;
+        if ((instruction & 0x8000) == 0)
+        {
+            // A-instruction: 0vvvvvvvvvvvvvvv
+            A = instruction & 0x7FFF;
+            PC++;
+            return;
+        }
 
-        ram.Write(16835, value);
-        ram.Write(16836, value);
-        ram.Write(16837, value);
-        ram.Write(16838, value);
+        // C-instruction: 111accccccdddjjj
+        int compBits = (instruction >> 6) & 0x3F;
+        int destBits = (instruction >> 3) & 0x07;
+        int jumpBits = instruction & 0x07;
 
-        toggle = !toggle;
+        int aluX = D;
+        int aluY = (instruction & (1 << 12)) != 0 ? M.Read(A) : A;
+
+        int no = compBits & 0x01;
+        int f = (compBits >> 1) & 0x01;
+        int ny = (compBits >> 2) & 0x01;
+        int zy = (compBits >> 3) & 0x01;
+        int nx = (compBits >> 4) & 0x01;
+        int zx = (compBits >> 5) & 0x01;
+
+        var (outValue, zr, ng) = ALU((zx, nx, zy, ny, f, no), aluX, aluY);
+
+        // Handle destination bits
+        if ((destBits & 0b100) != 0) A = outValue;
+        if ((destBits & 0b010) != 0) D = outValue;
+        if ((destBits & 0b001) != 0) M.Write(A, outValue);
+
+        // Handle jump bits
+        bool jump = ShouldJump(jumpBits, outValue, zr == 1, ng == 1);
+        PC = jump ? A : PC + 1;
+    }
+
+    private bool ShouldJump(int jumpBits, int outValue, bool zero, bool negative)
+    {
+        return jumpBits switch
+        {
+            0b001 => outValue > 0,       // JGT
+            0b010 => zero,               // JEQ
+            0b011 => outValue >= 0,      // JGE
+            0b100 => negative,           // JLT
+            0b101 => !zero,              // JNE
+            0b110 => outValue <= 0,      // JLE
+            0b111 => true,               // JMP
+            _ => false
+        };
+    }
+
+    private static (int output, int zr, int ng) ALU((int zx, int nx, int zy, int ny, int f, int no) controlBits, int x, int y)
+    {
+        var (zx, nx, zy, ny, f, no) = controlBits;
+
+        // Apply (negation . zero) to x and y
+        x = zx == 1 ? 0 : x;
+        x = nx == 1 ? ~x : x;
+        y = zy == 1 ? 0 : y;
+        y = ny == 1 ? ~y : y;
+
+        // Calculate the output
+        int output = f == 1 ? x + y : x & y;
+        output = no == 1 ? ~output : output;
+
+        // Calculate zr (zero flag) and ng (negative flag)
+        int zr = output == 0 ? 1 : 0;
+        int ng = output < 0 ? 1 : 0;
+
+        return (output, zr, ng);
     }
 }
--- src/Computing.UI/src/Computer.cs
+++ src/Computing.UI/src/Computer.cs
@@ -7,8 +7,27 @@ using ComputingSystem.Components;
 public partial class Computer : Node2D
 {
     private RAM _ram = new();
-    private ROM _rom = new(new int[32678]);
     private CPU _cpu = new();
+
+    private ROM _rom;
+
+    public Computer()
+    {
+        // TODO: will eventually load this from an external location
+        // could be a file? text input? dynamically compiled hack asm? 
+        int[] instructions = [
+            0b0000000000000010, // @2
+            0b1110110000010000, // D=A
+            0b0000000000000011, // @3
+            0b1110000010010000, // D=A+D
+            0b0000000000000000, // @0
+            0b1110001100001000, // M=D
+            0b0000000000000110, // @6
+            0b1110101010000111, // 0;JMP
+        ];
+
+        _rom = new ROM(instructions);
+    }
 
     [Signal]
     public delegate void ScreenUpdateEventHandler(int[] pixels);
@@ -18,6 +37,9 @@ public partial class Computer : Node2D
         int instruction = _rom.Read(_cpu.PC);
         _cpu.Execute(instruction, _ram);
 
-        EmitSignal("ScreenUpdate", _ram.ReadScreen());
+        // TODO: needs some more robust debugging and visualization in UI
+        Debug.WriteLine($"A: {_cpu.A}, D: {_cpu.D}, PC: {_cpu.PC}, RAM[0]: {_ram.Read(0)}");
+
+        EmitSignal(SignalName.ScreenUpdate, _ram.ReadScreen());
     }
 }
